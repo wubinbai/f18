08c2136cf90e3db6788e4442e0c3fe3fe13baa59
Wubin Bai <wubinbai@yahoo.com>
2019-02-22 17:30:49 +0800

    digit scores

--- python/book/handson-ml-master/wubin_practice/digit.py
+++ python/book/handson-ml-master/wubin_practice/digit.py
@@ -9,4 +9,26 @@ some_digit = X[n]
 para1 = int(some_digit.shape[0]**0.5)
 some_digit_image = some_digit.reshape(para1,para1)
 plt.imshow(some_digit_image,cmap = matplotlib.cm.binary, interpolation = "nearest")
+
 plt.show()
+
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y)
+sgd = SGDClassifier()
+# Binary SGD:
+sgd_5 = SGDClassifier()
+y_train_5 = (y_train==5)
+y_test_5 = (y_test==5)
+sgd.fit(X_train,y_train)
+sgd_5.fit(X_train,y_train_5)
+
+predict = sgd.predict(X_test)
+predict_5 = sgd_5.predict(X_test)
+from sklearn.metrics import confusion_matrix
+cf=confusion_matrix(y_test,predict)
+cf_5=confusion_matrix(y_test_5,predict_5)
+from sklearn.metrics import precision_score, recall_score, f1_score
+ps5 = precision_score(y_test_5,predict_5)
+rs5 = recall_score(y_test_5,predict_5)
+f1s5 = f1_score(y_test_5,predict_5)
